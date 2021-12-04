62a27d4a7193ec267008c8cce33237c2f6cfc9d1
nickolasbmm <nickolasbmm44@gmail.com>
2021-12-04 16:40:43 -0300

    ordem certa das questoes

--- q7.pl
+++
@@ -1,5 +0,0 @@
-ultimo([X],X).
-ultimo([_|T],W):-ultimo(T,W).
-
-palindromo(L):-length(L,1).
-palindromo([X|T]):-ultimo(T,W),X=W.
--- q8.pl
+++ q8.pl
@@ -1,8 +1,5 @@
-testa_primo(_,0,0).
-testa_primo(_,1,1).
-testa_primo(N,_,0):-0=:=mod(N,2),N>2.
-testa_primo(N,X,0):-X=\=0,0=:=mod(N,X).
-testa_primo(N,X,W):-X=\=0,0=\=mod(N,X),X1 is X-1,testa_primo(N,X1,W).
+ultimo([X],X).
+ultimo([_|T],W):-ultimo(T,W).
 
-primo(N):-W is N-1,testa_primo(N,W,1).
-
+palindromo(L):-length(L,1).
+palindromo([X|T]):-ultimo(T,W),X=W.
